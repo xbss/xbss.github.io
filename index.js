4d76b6049fe35f1b0dfd97b2747b46472c9eff0d
XBSS <1360303452@qq.com>
2016-11-28 19:15:54 +0800

    edit 修改首页 和文件布局

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+// 加载完后执行
+$(document).ready(function(){
+	// 搜索框
+	$("#top-search input").focusin(function(){ // 更改css
+		$("#top-search input").css({
+			'background': '#eee'
+		});
+		$("#top-search").animate({
+			'margin-left':'-60px'
+		},0);
+	});
+	$("#top-search input").focusout(function(){ // 更改css
+		$("#top-search input").css({
+			'background': 'url("search.png") no-repeat',
+			'background-size':'contain'
+		});
+		$("#top-search").animate({
+			'margin-left':'0px'
+		},0);
+	})
+	
+	// 头像菜单事件
+	$("#avater-container div").mouseover(function(){
+		$("#avater-nav").fadeIn();
+	});
+	$("#avater-menu").mouseleave(function(){
+		$("#avater-nav").fadeOut();
+	});
+	/*$("#avater-container").fadeOut();*/
+	$("#avater-container").click(function(){ // 点击回到顶部
+		$('html,body').animate({scrollTop:0},500);
+	});
+	$(document).scroll(function(){ //绑定滚动事件，滚动到200px时显示头像菜单
+		var document_top =  $(document).scrollTop();
+		if(document_top >= 200)  $("#avater-container").fadeIn(0.1);
+		else $("#avater-container").fadeOut(0.1);
+	});
+});
